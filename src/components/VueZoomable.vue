f051af0b2c6f91addaea074542ed7b1302f69f91
Lars Noack <lars@webcontact.de>
2023-08-03 11:54:02 +0200

    feat: prepared injection

--- src/components/VueZoomable.vue
+++ src/components/VueZoomable.vue
@@ -1,25 +1,20 @@
 
 <template>
-    <div
-        ref="container"
-        class="container"
-        :class="$style.container"
-        @mousedown="mouse.onMouseDown"
-        @dblclick="mouse.onDblClick"
-        @touchstart="touch.onTouchStart"
-        @wheel="wheel.onWheel"
-    >
+    <div ref="container" class="container" :class="$style.container" @mousedown="mouse.onMouseDown"
+        @dblclick="mouse.onDblClick" @touchstart="touch.onTouchStart" @wheel="wheel.onWheel" @mouseleave="onMouseLeave">
         <slot />
     </div>
 </template>
 <script setup lang="ts">
-import { computed, ref, Ref, createApp } from 'vue';
+import { computed, ref, Ref, createApp, provide } from 'vue';
 import { onMounted, watch } from 'vue';
 import { useMouse } from "../composables/useMouse";
 import { useTouch } from "../composables/useTouch";
 import { useWheel } from "../composables/useWheel";
 
 import ScrollOverlay from './ScrollOverlay.vue';
+
+const hideOverlay: Ref<boolean> = ref(false);
 
 let emit = defineEmits(["panned", "zoom"]);
 let props = defineProps({
@@ -118,17 +113,28 @@ watch(transform, () => {
 
 onMounted(() => {
     const placeholder = document.createElement('div');
-    createApp(ScrollOverlay, {enableWheelOnKey: props.enableWheelOnKey }).mount(placeholder);
+    const scrollOverlayApp = createApp(ScrollOverlay, { enableWheelOnKey: props.enableWheelOnKey });
+
+    // needs to be injected before it is mounted
+    scrollOverlayApp.provide("hideOverlay", { hideOverlay });
+
+    scrollOverlayApp.mount(placeholder)
     container.value.appendChild(placeholder);
 
     setTransform();
 });
+
 
 const pressedKeys: Ref<Set<String>> = ref(new Set());
 
 // track the keys, which are currently pressed
 document.addEventListener('keydown', (event) => { pressedKeys.value.add(event.key); });
 document.addEventListener('keyup', (event) => { pressedKeys.value.delete(event.key); });
+
+// track when the mouse leaves, to then hide the overlay
+function onMouseLeave(event: MouseEvent) {
+    hideOverlay.value = true;
+}
 
 let mouse = useMouse(props, emit, pan, zoom, pressedKeys);
 let touch = useTouch(props, emit, pan, zoom, pressedKeys);
