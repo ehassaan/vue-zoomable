d897c46b3f84a40bab05d7701479268f8905dea7
Lars Noack <lars@webcontact.de>
2023-08-07 09:57:52 +0200

    fix: fixed bug, where zoom gets stuck with wheel at some occation

--- src/composables/useWheel.ts
+++ src/composables/useWheel.ts
@@ -9,31 +9,36 @@ export function useWheel(
     showOverlay: Function) {
 
     function onWheel(ev: WheelEvent) {
+        // check if all conditions are met to scroll
         if (!props.wheelEnabled || !props.zoomEnabled) return;
-        let newZoom = zoom.value + (props.dblClickZoomStep * ev.deltaY / Math.abs(ev.deltaY));
         if (props.enableWheelOnKey !== undefined && !pressedKeys.value.has(props.enableWheelOnKey)) {
             showOverlay();
             return;
         }
 
-        // it would zoom, if the "zoom bounds" aren't exeded
-        if (newZoom > props.maxZoom) zoom.value = props.maxZoom;
-        else if (newZoom == props.maxZoom || newZoom == props.minZoom || isNaN(newZoom)) return;
-        else if (newZoom < props.minZoom) zoom.value = props.minZoom;
-        else zoom.value = newZoom;
-        console.log("Wheel: ", newZoom, ev);
+        // calculate the new zoom value including all the bounds and store the old value for later compare if an event should be sent
+        const oldZoom = zoom.value;
+        let newZoom = zoom.value + (props.dblClickZoomStep * ev.deltaY / Math.abs(ev.deltaY));
+        if (newZoom > props.maxZoom) newZoom = props.maxZoom;
+        else if (newZoom < props.minZoom) newZoom = props.minZoom;
 
-        let event: ZoomableEvent = {
-            zoom: zoom.value,
-            pan: {
-                x: pan.value.x,
-                y: pan.value.y,
-                deltaX: 0,
-                deltaY: 0,
-            },
-            type: "wheel"
-        };
-        emit("zoom", event);
+        // check if it should sent an update, and do so
+
+        if (oldZoom !== newZoom) {
+            zoom.value = newZoom;
+
+            let event: ZoomableEvent = {
+                zoom: zoom.value,
+                pan: {
+                    x: pan.value.x,
+                    y: pan.value.y,
+                    deltaX: 0,
+                    deltaY: 0,
+                },
+                type: "wheel"
+            };
+            emit("zoom", event);
+        }
     }
 
     return {
